a519f5d7c3e01c6f785a85a57de4da3c88751445
Hairul <khairullah.aplikasi@gmail.com>
2025-09-24 07:56:02 +0700

    feat middleware

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    { path: '/', redirect: '/home' },
+    { path: '/', redirect: '/login' },
     { path: '/register', name: 'Register', component: () => import('../views/RegisterView.vue') },
     { path: '/login', name: 'Login', component: () => import('../views/LoginView.vue') },
     { path: '/home', name: 'Home', component: () => import('../views/HomeView.vue') },
@@ -11,6 +10,15 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const user = localStorage.getItem('user');
+    if (!user && to.name !== 'Login' && to.name !== 'Register') {
+        next({ name: 'Login' });
+    } else {
+        next();
+    }
 });
 
 export default router;
